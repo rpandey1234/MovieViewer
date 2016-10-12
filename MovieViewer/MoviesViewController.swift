4820809d19eb5fe38ac09bbcadd2b4d49e9069a1
Rahul Pandey <rahul@pinterest.com>
2016-10-12 00:12:51 -0700

    finish image loading

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -55,14 +56,20 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
+        let posterPath = movie["poster_path"] as? String
         
-        let baseUrl = "http://image.tmdb.org/t/p/w500"
-        let imageUrl = NSURL(string: baseUrl + posterPath)
+        
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
-//        cell.posterView =
+        if let posterPathValid = posterPath {
+            let baseUrl = "http://image.tmdb.org/t/p/w500"
+            let imageUrl = NSURL(string: baseUrl + posterPathValid)
+            cell.posterView.setImageWithURL(imageUrl!)
+        } else {
+            // do nothing
+        }
+        
         print("row \(indexPath.row), index at position \(indexPath.indexAtPosition(2))")
         return cell
     }
