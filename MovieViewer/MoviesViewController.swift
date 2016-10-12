286568fe70229e989f1967122bd524919aea77d9
Rahul Pandey <rahul@pinterest.com>
2016-10-11 23:57:14 -0700

    working on showing images

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -55,9 +55,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
+        
+        let baseUrl = "http://image.tmdb.org/t/p/w500"
+        let imageUrl = NSURL(string: baseUrl + posterPath)
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+//        cell.posterView =
         print("row \(indexPath.row), index at position \(indexPath.indexAtPosition(2))")
         return cell
     }
